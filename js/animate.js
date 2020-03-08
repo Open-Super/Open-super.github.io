a9e69a80e19a6710bc924f1bcaae16edfaf6464b
YUTONGWU <superxzlove@qq.com>
2020-03-08 11:06:40 +0800

    Site updated: 2020-03-08 11:06:40

--- js/animate.js
+++ js/animate.js
@@ -1,3 +1,3 @@
-// build time:Sat Mar 07 2020 09:57:34 GMT+0800 (GMT+08:00)
+// build time:Sun Mar 08 2020 11:06:33 GMT+0800 (GMT+08:00)
 $(function(){if(mihoConfig.isHome===true&&document.body.clientWidth>700){var e=["pulse","fadeInRight","flipInX","lightSpeedIn","rotateInUpLeft","slideInUp","zoomIn"],a=e.length,n=e[Math.ceil(Math.random()*a)-1];window.sr=ScrollReveal();sr.reveal("#header-row",{duration:0,afterReveal:function(e){$(e).addClass("animated "+n)}});sr.reveal(".body-wrap > article",{duration:0,afterReveal:function(e){$(e).addClass("animated "+n)}})}});
 //rebuild by neat 
