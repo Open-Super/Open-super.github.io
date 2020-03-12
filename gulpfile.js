c2c3d23aec2baa03ccb5dc5c0964f06240eb1abf
YUTONGWU <superxzlove@qq.com>
2020-03-12 21:34:03 +0800

    Site updated: 2020-03-12 21:34:02

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,3 @@
-// build time:Thu Mar 12 2020 20:12:37 GMT+0800 (GMT+08:00)
+// build time:Thu Mar 12 2020 21:33:53 GMT+0800 (GMT+08:00)
 var gulp=require("gulp");var minifycss=require("gulp-minify-css");var uglify=require("gulp-uglify");var minifyhtml=require("gulp-minify-html");gulp.task("minify-css",function(){return gulp.src("./public/**/*.css").pipe(minifycss()).pipe(gulp.dest("./public"))});gulp.task("minify-html",function(){return gulp.src("./public/**/*.html").pipe(minifyhtml()).pipe(gulp.dest("./public"))});gulp.task("minify-js",function(){return gulp.src(["./public/**/*.js","!./public/lib/blog-encrypt.js"]).pipe(uglify()).pipe(gulp.dest("./public"))});gulp.task("default",gulp.parallel("minify-css","minify-html","minify-js"));
 //rebuild by neat 
