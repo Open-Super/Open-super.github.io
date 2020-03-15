a9a5d30d94c6b5e37593769968c0b2587efe8d23
YUTONGWU <superxzlove@qq.com>
2020-03-15 09:50:26 +0800

    Site updated: 2020-03-15 09:50:21

--- pace/test/simple.js
+++ pace/test/simple.js
@@ -1,3 +1,3 @@
-// build time:Sun Mar 15 2020 09:45:10 GMT+0800 (GMT+08:00)
+// build time:Sun Mar 15 2020 09:50:03 GMT+0800 (GMT+08:00)
 var total=5e4,count=0,pace=require("../")({total:total,showBurden:true,maxBurden:.5});while(count++<total){pace.op();for(var i=0;i<1e6;i++){count=count}}
 //rebuild by neat 
