e708470e884e7ed01afd7fb217423408df7a2e06
YUTONGWU <superxzlove@qq.com>
2020-03-15 09:51:06 +0800

    Site updated: 2020-03-15 09:51:05

--- pace/test/advanced.js
+++
@@ -1,3 +0,0 @@
-// build time:Sun Mar 15 2020 09:50:03 GMT+0800 (GMT+08:00)
-var total=5e4,current=0,pace=require("../")(total);while(current++<total){if(Math.random()>.9){pace.op(current)}if(Math.random()<.05&&total<=5e4){total+=Math.floor(Math.random()*100);pace.total=total}for(var i=0;i<1e6;i++){current=current}}
-//rebuild by neat 
