1d135edd42c8621f69245962ab4311b16a73cee7
YUTONGWU <superxzlove@qq.com>
2020-03-29 13:50:42 +0800

    Site updated: 2020-03-29 13:50:42

--- js/FunnyTitle.js
+++
@@ -1,3 +0,0 @@
-// build time:Sat Mar 28 2020 17:35:46 GMT+0800 (GMT+08:00)
-var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/funny.ico");document.title="╭(°A°`)╮ 页面崩溃啦 ~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/favicon.ico");document.title="(ฅ>ω<*ฅ) 噫又好啦 ~"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
-//rebuild by neat 
